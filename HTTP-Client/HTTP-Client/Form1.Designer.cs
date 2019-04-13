bd961f4e715f21f8918f31dc1dd36cceb497f89b
= <andrushko.egor@gmail.com>
2019-04-13 17:01:26 +0300

    Create client

--- HTTP-Client/HTTP-Client/Form1.Designer.cs
+++ HTTP-Client/HTTP-Client/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.methodsComBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(423, 426);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(483, 39);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // methodsComBox
+            // 
+            this.methodsComBox.FormattingEnabled = true;
+            this.methodsComBox.Location = new System.Drawing.Point(483, 12);
+            this.methodsComBox.Name = "methodsComBox";
+            this.methodsComBox.Size = new System.Drawing.Size(75, 21);
+            this.methodsComBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.methodsComBox);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ComboBox methodsComBox;
     }
 }
 
--- HTTP-Client/HTTP-Client/Form1.cs
+++ HTTP-Client/HTTP-Client/Form1.cs
@@ -5,16 +5,54 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows.Forms;
 
 namespace HTTP_Client
 {
     public partial class Form1 : Form
     {
+        const string hr = "\r\n===================================\r\n";
         public Form1()
         {
             InitializeComponent();
+            methodsComBox.Items.Add(HttpMethod.Get);
+            methodsComBox.Items.Add(HttpMethod.Post);
+            methodsComBox.Items.Add(HttpMethod.Head);
+            methodsComBox.Items.Add(HttpMethod.Delete);
+            methodsComBox.Items.Add(HttpMethod.Put);
+            methodsComBox.SelectedIndex = 0;
+        }
+
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            SendRequestAsync();
+        }
+        private async void SendRequestAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string uriTextbox = "http://www.google.com/";
+                    HttpRequestMessage request = new HttpRequestMessage(methodsComBox.SelectedItem as HttpMethod, uriTextbox)
+                    {
+                        Version = Version.Parse("1.0"),
+                    };
+                    textBox1.Text += $"{request.Method} {request.RequestUri} HTTP/{request.Version}{hr}";
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    textBox1.Text += $"HTTP/{response.Version} {response.StatusCode}{hr}";
+                    textBox1.Text += response.Headers + hr;
+                    textBox1.Text += await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
         }
     }
 }
